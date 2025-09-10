d45787234e5fccdebc7ea91b6bab5119e2c8e220
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2025-09-09 23:21:52 -0300

    .

--- 17 - Request Params/src/main/java/__SpringBoot2/__star_Spring_io/mapper/AnimeMapper.java
+++ 17 - Request Params/src/main/java/__SpringBoot2/__star_Spring_io/mapper/AnimeMapper.java
@@ -1,6 +1,7 @@
 package __SpringBoot2.__star_Spring_io.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import __SpringBoot2.__star_Spring_io.dominio.Anime;
@@ -10,10 +11,11 @@ import __SpringBoot2.__star_Spring_io.requests.AnimePutRequestBody;
 //(componentModel = "spring") e INTANCE tem a msm serventia porem intance pode ser usado como metodo statico da clase para chamada de outros metodos
 //prefira ussar @Mapper quando for grandes projetos por comvençao
 
-@Mapper(componentModel = "spring")//ingeta dependencia no String para ussar vc precisa atribuir uma istancia "private final AnimeMapper"
-public abstract class AnimeMapper {
+@Mapper//(componentModel = "spring")//ingeta dependencia no String para ussar vc precisa atribuir uma istancia "private final AnimeMapper"
+public interface AnimeMapper {
 	public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);//ussa a istancia como metodo statico da clase
 	
+	@Mapping(target = "id", ignore = true) // ignora o campo id
 	public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
 	
 	public abstract Anime toAnime(AnimePutRequestBody animePutRequestBody);
