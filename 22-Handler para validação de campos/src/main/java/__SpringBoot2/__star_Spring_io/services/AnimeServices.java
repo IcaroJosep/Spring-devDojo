45017f02a08b1bf64aacd7d01419f1ea8a8f5b9f
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2025-09-17 18:04:23 -0300

    .

--- 22-Handler para validação de campos/src/main/java/__SpringBoot2/__star_Spring_io/services/AnimeServices.java
+++ 22-Handler para validação de campos/src/main/java/__SpringBoot2/__star_Spring_io/services/AnimeServices.java
@@ -1,19 +1,14 @@
 package __SpringBoot2.__star_Spring_io.services;
 
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import __SpringBoot2.__star_Spring_io.dominio.Anime;
+import __SpringBoot2.__star_Spring_io.exception.BedRequestException;
 import __SpringBoot2.__star_Spring_io.mapper.AnimeMapper;
 import __SpringBoot2.__star_Spring_io.repository.AnimeRepository;
 import __SpringBoot2.__star_Spring_io.requests.AnimePostRequestBody;
-import __SpringBoot2.__star_Spring_io.requests.AnimePutRequestBody;
 import jakarta.transaction.Transactional;
-import __SpringBoot2.__star_Spring_io.exception.BedRequestException;
 import lombok.RequiredArgsConstructor;
 
 @Service
