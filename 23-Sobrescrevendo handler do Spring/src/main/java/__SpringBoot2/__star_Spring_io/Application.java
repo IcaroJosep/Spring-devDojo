5e89e621c48bb1437c8c6457959df3c4513dc85d
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2025-09-21 20:51:54 -0300

    .

--- 23-Sobrescrevendo handler do Spring/src/main/java/__SpringBoot2/__star_Spring_io/Application.java
+++ 23-Sobrescrevendo handler do Spring/src/main/java/__SpringBoot2/__star_Spring_io/Application.java
@@ -10,10 +10,4 @@ public class Application {
 		 SpringApplication.run(Application.class, args);
 	}
 }
-/*
- * criaçao dos da clase BedRequestExceptionDetails em /exception 
- * tbm do pacote /handler com a classe RestExceptionHandler
- * 
- * oq ocoere e que rendler é uma espesse de comtroler q dis q toda ves e for retornado um determinada 
- * exeçao ela deve retornar o metodo indicado. 
- * */
+
