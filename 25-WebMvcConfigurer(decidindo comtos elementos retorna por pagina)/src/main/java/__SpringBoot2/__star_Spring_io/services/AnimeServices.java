f4d04d4e7f583a7eb485a79b4e8644dd98b29d1e
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2025-10-09 17:13:40 -0300

    .

---
+++ 25-WebMvcConfigurer(decidindo comtos elementos retorna por pagina)/src/main/java/__SpringBoot2/__star_Spring_io/services/AnimeServices.java
@@ -0,0 +1,26 @@
+package __SpringBoot2.__star_Spring_io.services;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import __SpringBoot2.__star_Spring_io.dominio.Anime;
+import __SpringBoot2.__star_Spring_io.repository.AnimeRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AnimeServices {
+	
+	private final AnimeRepository animeRepository;
+
+	
+	public Page<Anime> listAll(Pageable pageable) {
+		return  animeRepository.findAll(pageable);
+	}
+	
+	
+	
+
+}
