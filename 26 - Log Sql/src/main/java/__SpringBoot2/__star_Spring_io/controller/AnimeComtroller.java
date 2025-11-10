61187bfceb7b17b3d47840b0a4c346e3122b90ac
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2025-11-10 16:39:51 -0300

    .

---
+++ 26 - Log Sql/src/main/java/__SpringBoot2/__star_Spring_io/controller/AnimeComtroller.java
@@ -0,0 +1,38 @@
+package __SpringBoot2.__star_Spring_io.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import __SpringBoot2.__star_Spring_io.dominio.Anime;
+import __SpringBoot2.__star_Spring_io.services.AnimeServices;
+import __SpringBoot2.__star_Spring_io.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+
+@RequestMapping("animes")
+@Log4j2
+@RequiredArgsConstructor
+
+public class AnimeComtroller {
+	
+	private final DateUtil dateUtil; 
+	private final AnimeServices animeServices;
+	
+	
+	@GetMapping						//pageable resolver implementado no pecage comfigurer para aceitar pageble com parametros!!
+	public ResponseEntity<Page<Anime>> list(Pageable pageable){
+		log.info(dateUtil.formatLocalDataTimeToDatabaseStyle(LocalDateTime.now()));
+		return ResponseEntity.ok(animeServices.listAll(pageable));
+	}
+	//por padrao pageble ja aceita requisicoes comdendo page,size e sort
+	
+}
+
