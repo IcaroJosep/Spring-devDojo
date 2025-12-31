6c56b3debdb843e8d177f119c3a7b38cd20d5252
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2025-12-31 09:36:51 -0300

    .

--- 29-cliente-base-RestTemplate-post/src/main/java/main/Anime.java
+++ 29-cliente-base-RestTemplate-post/src/main/java/main/Anime.java
@@ -15,7 +15,7 @@ public class Anime {
 	@Positive
 	private int id;
 	
-	@Size(max=100 , message = "nome com mais de 100 caracterers nao serao aceitos")
+	@Size(max=100 ,min = 1, message = "nome com mais de 100 caracterers nao serao aceitos")
 	private String name; 
 	
 }
