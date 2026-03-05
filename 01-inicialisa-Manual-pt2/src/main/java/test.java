35f4c523c127b852f4d15261c6bc43cb181a1681
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2026-03-05 19:23:00 -0300

    .

--- 01-inicialisa-Manual-pt2/src/main/java/test.java
+++ 01-inicialisa-Manual-pt2/src/main/java/test.java
@@ -1,9 +1,19 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+	    public static void main(String[] args)  {        
+	        System.out.println(System.getProperty("os.name"));
+	        System.out.println(System.getProperty("device.name"));
+	        try {
+				System.out.println(InetAddress.getLocalHost().getHostName());
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	    }
+	
 }
